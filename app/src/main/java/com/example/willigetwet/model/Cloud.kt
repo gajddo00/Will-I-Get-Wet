a2b14edbd36b7e1799aec51f1190b8a91d8562aa
gajddo00 <dominika.gajdova@gnj.cz>
2020-12-04 19:13:11 +0100

    update

---
+++ app/src/main/java/com/example/willigetwet/model/Cloud.kt
@@ -0,0 +1,8 @@
+package com.example.willigetwet.model
+
+data class Cloud (
+    var id: Int,
+    var name: String,
+    var description: String,
+    var rainProbability: String
+)
--- app/src/main/java/com/example/willigetwet/ui/cloudsbook/CloudsFragment.kt
+++ app/src/main/java/com/example/willigetwet/ui/cloudsbook/CloudsFragment.kt
@@ -6,19 +6,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.willigetwet.R
+import com.example.willigetwet.model.Cloud
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.File
 
 class CloudsFragment : Fragment() {
+
+    var clouds: List<Cloud> = listOf<Cloud>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
 
+    fun getClouds() {
+        val filePath = resources.openRawResource(R.raw.data).toString()
+        val json: String = File(filePath).readText(Charsets.UTF_8)
+        val sType = object : TypeToken<List<Cloud>>() { }.type
+        clouds = Gson().fromJson<List<Cloud>>(json, sType)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_clouds, container, false)
+        val view = inflater.inflate(R.layout.fragment_clouds, container, false)
+        getClouds()
+
+        return view
     }
 }
